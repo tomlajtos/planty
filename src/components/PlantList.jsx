c35ff4074a2924e3ac6b9b1dda47a4efaa6831e8
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-04-14 23:53:04 +0200

    Add test components: PlantCard, PlantList

---
+++ src/components/PlantList.jsx
@@ -0,0 +1,9 @@
+import { PlantCard } from "./PlantCard.jsx";
+
+export const PlantList = ({plants}) => {
+  return (
+    <ul className="mx-auto p-4 flex flex-row flex-wrap gap-4 justify-center w-full max-w-5xl ">
+    { plants.map(plant => <PlantCard key={plant.id} plant={plant}/>)}
+    </ul>
+  );
+};
