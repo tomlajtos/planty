2bc4ccd3afad14a77f9614e816d9b4ddb2c62c9a
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-04-14 23:54:40 +0200

    Add test data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { PlantList } from './components/PlantList.jsx'
+import { myPlants } from "./utils/test-data.js";
 
 function App() {
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Plant Manager</h1>
-      <div className="card">
-        <p>
-    Manage your house plants.
-        </p>
+      <h1 className="text-3xl font-bold mb-4">Plant Manager</h1>
+      <div className='mx-auto py-3 text-2xl align-center mb-4 h-16 w-full min-w-fit max-w-5xl bg-purple-200'>
+        Menu: add, select, etc.
       </div>
+      {myPlants && (<PlantList plants={myPlants} />)}
     </div>
   )
 }
