7be6af150dd8f64905532b5426c0c704c8295b14
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-04-16 22:17:11 +0200

    Test Button and PlantForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
-import './App.css'
-import { PlantList } from './components/PlantList.jsx'
+
 import { myPlants } from "./utils/test-data.js";
+import './App.css'
+
+import { PlantList } from './components/PlantList.jsx'
+import { Button } from "./components/ui/Button"
+import { PlantForm } from './components/PlantForm';
 
 function App() {
 
   return (
     <div className="App">
       <h1 className="text-3xl font-bold mb-4">Plant Manager</h1>
-      <div className='mx-auto py-3 text-2xl align-center mb-4 h-16 w-full min-w-fit max-w-5xl bg-purple-200'>
-        Menu: add, select, etc.
+      <PlantForm submitFn={(e) => {
+        e.preventDefault();
+        console.log("form submitted")
+
+      }} />
+      <div className='mx-auto py-3 text-2xl align-center mb-4 h-16 w-full max-w-5xl bg-purple-200'>
+        Menu-buttons:
+        <Button className="mx-2 w-11 h-11  bg-emerald-900 rounded-lg text-3xl text-white" type={"button"} text={"+"} clickFn={() => alert("Click-click-clik")} />
       </div>
       {myPlants && (<PlantList plants={myPlants} />)}
     </div>
