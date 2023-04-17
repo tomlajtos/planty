1c158d1a3dc0f8f139b8a6af938382a8b7180bfe
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-04-17 12:49:26 +0200

    Refactor component: PlantForm

--- src/components/PlantForm.jsx
+++ src/components/PlantForm.jsx
@@ -1,15 +1,12 @@
 import { Button } from "./ui/Button"
+import { FormTextInput } from "./ui/FormTextInput";
+
 export const PlantForm = ({ submitFn }) => {
   return (
     <form className="p-4 mx-auto my-4 flex flex-col gap-3 items-tart text-left border border-slate-500 rounded-2xl max-w-md" onSubmit={submitFn}>
-      <div className="w-full grid grid-cols-6 grid-rows-1">
-        <label htmlFor="name" className="col-span-2">Name: </label>
-        <input id="name" className="col-span-4 bg-slate-100 w-full max-w-md border border-black rounded-full" name="name" type="text" />
-      </div>
-      <div className="w-full grid grid-cols-6 grid-rows-1">
-        <label htmlFor="sciName" className="col-span-2">Scientific name: </label>
-        <input id="sciName" className="col-span-4 bg-slate-100 w-full max-w-md border border-black rounded-full" name="name" type="text" />
-      </div>
+          <FormTextInput htmlFor={"name"} labelText={"Name: "} id={"name"} name={"name"} changeFn={(e)=> console.log(e.target.value)}/>
+          <FormTextInput htmlFor={"sciName"} labelText={"Scientific Name: "} id={"sciName"} name={"sciName"} changeFn={(e)=> console.log(e.target.value)}/>
+          <FormTextInput htmlFor={"nameT"} labelText={"Text input test: "} id={"nameT"} name={"nameT"} changeFn={(e)=> console.log(e.target.value)}/>
       <label>
         Date of purchase: <input type="date" name="purchaseDate" />
       </label>
