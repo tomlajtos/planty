97e1c900df2fda465c4481438430bc6c58bf1ce1
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-16 18:36:23 +0200

    Re-style button

--- src/components/ui/SquareButton.jsx
+++ src/components/ui/SquareButton.jsx
@@ -1,6 +1,6 @@
 export const SquareButton = ({ type, text, className, clickFn }) => {
   const fixedClasses =
-    "mx-2 px-4 py-1 w-fit h-fit rounded-full shadow-md text-2xl text-slate-800";
+    "mx-2 px-4 py-1 w-fit h-fit rounded-full shadow-md text-xl text-slate-800";
   const classNames = fixedClasses + " " + className;
   return (
     <button className={classNames} type={type} onClick={clickFn}>
